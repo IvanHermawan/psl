116d74e87918634e8ddbf7d5e1b44e8b8d790ddc
Ivan Hermawan <ivanhermawan19@gmail.com>
2022-07-31 10:01:51 +0700

    go version upgrade

--- src/com/golang_version_upgrade/main.groovy
+++ src/com/golang_version_upgrade/main.groovy
@@ -3,12 +3,10 @@ package com.golang_version_upgrade
 def main(script) {
     //def GO_VERSION = ("${script.env.GO_VERSION}" != "null") ? "${script.env.GO_VERSION}" : ""
 
-    ansiColor('xterm') {
-        stage('GO-Version'){
-            def root = tool type: "go", name: "${GO_VERSION}"
-            withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
-                sh 'go version'
-            }
-        }  
-    }
+    stage('GO-Version'){
+        def root = tool type: "go", name: "${GO_VERSION}"
+        withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
+            sh 'go version'
+        }
+    }  
 }
